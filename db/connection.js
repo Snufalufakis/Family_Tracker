c698d4cdc09a679fb6c1736224c820d33dd20a8a
Michael Harris <snufalufakis@gmail.com>
2022-07-26 21:06:12 -0700

    modified files to get node start to work

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
-const mysql = require('mysql2');
+import inquirer from "inquirer";
+import mysql from "mysql2";
+
 const connection = mysql.createConnection({
 host: 'localhost',
 user: process.env.DB_USER,
 password: process.env.DB_PASSWORD,
 database: 'family_db',
 }).promise();
-module.exports = connection;
+export default connection;
--- server.js
+++ server.js
@@ -1,47 +1,72 @@
-const inq = import('inquirer');
-const mysql = require('mysql2');
-const cTable = require('console.table');
-const path = require('path');
-const connection = require('./db/connection')
-const add = require('./lib/add');
-const update = require('./lib/update');
-const view = require('./lib/view');
-
-const PORT = process.env.PORT || 3001
+import inquirer from "inquirer";
+import mysql from "mysql2";
+import cTable from "console.table";
+import path from "path";
+import connection from "./db/connection.js";
+import add from "./lib/add.js";
+import update from "./lib/update.js";
+import view from "./lib/view.js";
 
 
-connection.connect = () => {
-if (err) throw err;
-console.log('Seems like you ready');
-exports.start();
-}
-exports.start =() => {
-    inq.prompt([
-        {
-            type: 'list',
-            message: 'What would you like to see?',
-            name: 'choices',
-            choices: [
-                'View All Family Members',
-                'Add Family Members',
-                'Update Family Members',
-                'Exit'
-            ]
-        }
-    ])
-    .then(function(answers){
-        if (answers.choice === 'View All Family Members') {
-            view.viewAllFamilyMembers();
-        }
-        else if(answers.choice === 'Add Family Members') {
-            add.addFamilyMembers();
-        }
-        else if(answers.choice === 'Update Family Members') {
-            update.updateFamilyMembers();
-        }
-        else if(answers.choice === 'Exit') {
-            connection.close();
-            return
-        }
-    })
-};
+const PORT = process.env.PORT || 3001;
+
+console.log(inquirer);
+inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to see?",
+      name: "choices",
+      choices: [
+        "View All Family Members",
+        "Add Family Members",
+        "Update Family Members",
+        "Exit",
+      ],
+    },
+  ])
+  .then(function (answers) {
+    if (answers.choice === "View All Family Members") {
+      view.viewAllFamilyMembers();
+    } else if (answers.choice === "Add Family Members") {
+      add.addFamilyMembers();
+    } else if (answers.choice === "Update Family Members") {
+      update.updateFamilyMembers();
+    } else if (answers.choice === "Exit") {
+      connection.close();
+      return;
+    }
+  });
+
+  
+// function start () {
+
+//     inquirer.prompt([
+//         {
+//             type: 'list',
+//             message: 'What would you like to see?',
+//             name: 'choices',
+//             choices: [
+//                 'View All Family Members',
+//                 'Add Family Members',
+//                 'Update Family Members',
+//                 'Exit'
+//             ]
+//         }
+//     ])
+//     .then(function(answers){
+//         if (answers.choice === 'View All Family Members') {
+//             view.viewAllFamilyMembers();
+//         }
+//         else if(answers.choice === 'Add Family Members') {
+//             add.addFamilyMembers();
+//         }
+//         else if(answers.choice === 'Update Family Members') {
+//             update.updateFamilyMembers();
+//         }
+//         else if(answers.choice === 'Exit') {
+//             connection.close();
+//             return
+//         }
+//     })
+// };
+// start();
